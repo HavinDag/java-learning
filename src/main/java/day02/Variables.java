6b93f18d994cbc479858a6c2d3a0ccf9388b187a
Havin Dag <havindag26@gmail.com>
2026-06-29 21:24:27 +0300

    Add Day01 - Variables

---
+++ src/main/java/day02/Variables.java
@@ -0,0 +1,23 @@
+package day02;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 13;
+        System.out.println(age);
+
+        char firstLetter = 'A';
+        boolean isRetired = false;
+        byte studentAge = 19;
+        short sitePopulation = 3000;
+        int countryPopulation = 40000000;
+        long cellCount = 9999999999999999L;
+        float shirtPrice = 199.99F;
+        double cellWeight = 0.000000000000000000112;
+        String name = "Daphne";
+
+
+        byte Note1 = 85;
+        byte Note2 = 90;
+        System.out.println(Note1+Note2);
+    }
+}
